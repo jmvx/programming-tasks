d45428606b6b0814374c246e526babcc2aa38460
Julia Van Cleve <julia@jmv.me>
2015-10-22 12:42:28 -0700

    Simple adding in C

---
+++ simple_adding/simple_adding.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int add(num) {
+  int sum = 0;
+  for (int i = 1; i <= num; i++) {
+    sum = sum + i;
+  }
+  return sum;
+}
+
+int main() {
+  printf("%d\n", add(12));
+  printf("%d\n", add(140));
+}
